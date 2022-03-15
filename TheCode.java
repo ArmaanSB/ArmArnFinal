2204cf025981f9304c2f382d954c94871a509beb
Arnav Deshpande <arnavd2014@gmail.com>
2022-03-15 12:34:05 -0400

    Runner and interface for printing quotient functionality created

---
+++ TheCode.java
@@ -0,0 +1,20 @@
+package ArmArnFinal;
+
+public class TheCode
+{
+  public TheCode()
+  {
+    printQuotient(3,7);
+  }
+  
+  public static void main(String[]args)
+  {
+    TheCode app = new TheCode();
+      System.out.println('Dsdsdsds');
+  }
+
+//  public void printQuotient(float x, float y){
+//      float quotient = x/y;
+//      printf(((float)((int)(quotient * 1000)))/1000);
+//   }
+}
